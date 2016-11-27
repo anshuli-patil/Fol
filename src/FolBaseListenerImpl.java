91c186391559548daf8c6fcdea246148e59aecdc
Anshuli Patil <anshulip@usc.edu>
2016-11-26 17:31:12 -0800

    exitNotSentence implemented

--- src/FolBaseListenerImpl.java
+++ src/FolBaseListenerImpl.java
@@ -1,7 +1,6 @@
 
 import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.TokenStreamRewriter;
-
 import edu.usc.anshulip.ai.hw3.Constants;
 
 public class FolBaseListenerImpl extends FolBaseListener {
@@ -30,6 +29,41 @@ public class FolBaseListenerImpl extends FolBaseListener {
 		rewriter.insertAfter(ctx.stop, replacement);
 		rewriter.delete(ctx.start, ctx.stop);
 		// rewriter.replace(ctx.start, ctx.stop, replacement);
+	}
+
+	@Override
+	public void exitNotSentence(FolParser.NotSentenceContext ctx) {
+		String replacement = null;
+		if (ctx.orSentence() != null) {
+			String leftSentence = ctx.orSentence().sentence(0).getText();
+			String rightSentence = ctx.orSentence().sentence(1).getText();
+			replacement = String.format("%c%c%c%s%c%c%c%c%s%c%c", Constants.OPENING_BRACE, Constants.OPENING_BRACE,
+					Constants.NOT, leftSentence, Constants.CLOSING_BRACE, Constants.AND, Constants.OPENING_BRACE,
+					Constants.NOT, rightSentence, Constants.CLOSING_BRACE, Constants.CLOSING_BRACE);
+
+		} else if (ctx.andSentence() != null) {
+			String leftSentence = ctx.andSentence().sentence(0).getText();
+			String rightSentence = ctx.andSentence().sentence(1).getText();
+			replacement = String.format("%c%c%c%s%c%c%c%c%s%c%c", Constants.OPENING_BRACE, Constants.OPENING_BRACE,
+					Constants.NOT, leftSentence, Constants.CLOSING_BRACE, Constants.OR, Constants.OPENING_BRACE,
+					Constants.NOT, rightSentence, Constants.CLOSING_BRACE, Constants.CLOSING_BRACE);
+
+		} else if (ctx.notSentence() != null) {
+			int length = ctx.notSentence().getText().length();
+			replacement = ctx.notSentence().getText().substring(2, length - 1);
+		} else {
+			if (ctx.literal().getText().startsWith(String.format("%c", Constants.NOT))) {
+				int length = ctx.literal().getText().length();
+				replacement = ctx.literal().getText().substring(2, length - 1);
+			} else {
+				String literal = ctx.literal().getText();
+				replacement = String.format("%c%c%s%c", Constants.OPENING_BRACE, Constants.NOT, literal,
+						Constants.CLOSING_BRACE);
+			}
+		}
+
+		rewriter.insertAfter(ctx.stop, replacement);
+		rewriter.delete(ctx.start, ctx.stop);
 	}
 
 }
